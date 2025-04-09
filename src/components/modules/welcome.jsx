7f0edf3f27d9eb63c7d80a5a7e775f2df28583b9
Alper Akkın <alperakkin@gmail.com>
2025-04-09 12:24:56 +0300

    handle welcome screen new file

--- src/components/modules/welcome.jsx
+++ src/components/modules/welcome.jsx
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import EditorLogo from "public/image/logo.png";
 import "public/css/welcome.css";
 
-const WelcomeScreen = (parser) => {
+const WelcomeScreen = ({ parser }) => {
+  const [command, setCommand] = useState("");
+  const [displayNewfile, setDisplayNewFile] = useState("none");
+  const isEventAdded = useRef(false);
+
+  useEffect(() => {
+    if (!parser || isEventAdded.current) return;
+    const handleKeyDown = (event) => {
+      if (command && event.key == "Enter") {
+        setDisplayNewFile("none");
+        console.log("Command is", command);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    isEventAdded.current = true;
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      isEventAdded.current = false;
+    };
+  }, [parser, command]);
+
   return (
     <div className="welcome-container">
       <div className="brand-info">
         <h1 className="brand-name">Blink</h1>
-        <p className="brand-description">Start coding with blink editor..</p>
+        <p className="brand-description">Let's start coding..</p>
         <div className="welcome-logo">
           <img src={EditorLogo} />
         </div>
       </div>
 
       <div className="welcome-actions">
-        <label className="new-file">New File</label>
+        <div className="new-file">
+          <label
+            className="new-file-label"
+            onClick={() => setDisplayNewFile("block")}
+          >
+            New File
+          </label>
+          <input
+            className="new-file-inp"
+            type="text"
+            value={command}
+            style={{ display: displayNewfile }}
+            onChange={(e) => setCommand(e.target.value)}
+          />
+        </div>
+
         <label className="recent-files">Recent Files</label>
         <label className="recent-folders">Recent Folders</label>
       </div>
