6a7be70862a693108559c47d9e84b741def1a705
Alper Akkın <alperakkin@gmail.com>
2025-04-14 18:30:46 +0300

    tab container added

--- src/components/modules/tab/tab_manager.jsx
+++ src/components/modules/tab/tab_manager.jsx
@@ -1,15 +1,15 @@
 import React, { forwardRef } from "react";
+import "public/css/tabmanager.css";
 
 const TabManager = forwardRef(({ activeFile, parser }, ref) => {
   return (
     <div
       className="tab-container"
       style={{
-        width: "100%",
-        height: "100%",
         display: activeFile ? "block" : "none",
       }}
     >
+      <div className="tab-viewer">Tabs will be rendered here</div>
       <div className="code-editor" ref={ref}></div>
     </div>
   );
